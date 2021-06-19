aec7f3f1d4d6ca1061496cf3e55bec02b03a03c4
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-19 13:06:05 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/springcloudstreamsfunctionalstyle/SpringCloudStreamsFunctionalStyleApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamsfunctionalstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamsFunctionalStyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamsFunctionalStyleApplication.class, args);
+    }
+
+}
